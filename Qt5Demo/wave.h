a393f3d067d349d4e497968e8d30228284b8b9fa
tudou <chuyueqiang@qq.com>
2023-05-23 03:47:32 +0800

    把三通道显示加上了，不过目前没法显示。

--- Qt5Demo/wave.h
+++ Qt5Demo/wave.h
@@ -2,6 +2,10 @@
 
 #include <QWidget>
 #include "ui_wave.h"
+#include "qvector.h"
+#include <QtCharts/qchartview.h>
+#include <QtCharts/qsplineseries.h>
+#include <qvalueaxis.h>
 
 class wave : public QWidget
 {
@@ -11,8 +15,29 @@ public:
 	wave(QWidget *parent = nullptr);
 	~wave();
 
+public:
+	QVector<quint32> ch1, ch2, ch3;
+
+	QChart* ch1_chart;
+	QSplineSeries* ch1_lineSeries_A;
+	QSplineSeries* ch1_lineSeries_B;
+	QValueAxis* ch1_axisX;
+	QValueAxis* ch1_axisY;
+
+	QChart* ch2_chart;
+	QSplineSeries* ch2_lineSeries_A;
+	QSplineSeries* ch2_lineSeries_B;
+	QValueAxis* ch2_axisX;
+	QValueAxis* ch2_axisY;
+
+	QChart* ch3_chart;
+	QSplineSeries* ch3_lineSeries_A;
+	QSplineSeries* ch3_lineSeries_B;
+	QValueAxis* ch3_axisX;
+	QValueAxis* ch3_axisY;
+
 public slots:
-	void on_pb_Start_clicked();
+	void on_pbStart_clicked();
 
 private:
 	//Ui::waveClass ui;
